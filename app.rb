0d5929e1d1c86e3c9e05a8a02a868fb0069c66f9
JawaraGordon <jawaragordon@icloud.com>
2022-06-03 19:28:33 -0400

    commit tree f72adf0822e14373ebba6cb39fabc2eee66c9663

---
+++ app.rb
@@ -0,0 +1,20 @@
+# app.rb
+puts "Hello World!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+
+print "Pass this test, please."
+
+# puts
+
+# puts [1, 2, 3]
+
+# puts [1, 2, 3].inspect
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
